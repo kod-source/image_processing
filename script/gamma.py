72b4ebce6c0d52bd9fab04f494a94a9ab7c09573
堀航大 <kodenginner4521@gmail.com>
2022-12-04 13:12:40 +0900

    カンマ調整するスクリプトの記述

---
+++ script/gamma.py
@@ -0,0 +1,35 @@
+import cv2
+import numpy as np
+
+file_name = 'dark_10k.jpg'
+
+# 画像読込
+img = cv2.imread("img/" + file_name)
+
+# """
+# **********************************************************
+# 【ガンマ補正の公式】
+#   Y = 255(X/255)**(1/γ)
+
+# 【γの設定方法】
+#   ・γ>1の場合：画像が明るくなる
+#   ・γ<1の場合：画像が暗くなる
+# **********************************************************
+# """
+
+# ガンマ変換用の数値準備
+gamma     = 3.0                               # γ値を指定
+img2gamma = np.zeros((256,1),dtype=np.uint8)  # ガンマ変換初期値
+
+# 公式適用
+for i in range(256):
+    img2gamma[i][0] = 255 * (float(i)/255) ** (1.0 /gamma)
+
+# 読込画像をガンマ変換
+gamma_img = cv2.LUT(img,img2gamma)
+
+# 画像を表示
+cv2.imshow("img",img)           # オリジナル画像
+cv2.imshow("gamma",gamma_img)   # ガンマ変換後の画像
+cv2.waitKey(0)
+cv2.destroyAllWindows()
